4e934c099df9c07c1036757c2f4e52ac12a80790
v.vasin <v.vasin@action-media.ru>
2019-04-19 14:23:00 +0300

    тест по авторизации админки

--- src/test/java/learning/FirstTest.java
+++ src/test/java/learning/FirstTest.java
@@ -3,20 +3,33 @@ package learning;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
 public class FirstTest {
-    private WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeTest
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
 
     @Test
-    public void myFirstTest() {
-        driver.navigate().to("http://www.google.com");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnG")).click();
+    public void Auth_test() {
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
 
+    @AfterTest
+    public void stop()  {
+        driver.quit();
+        driver = null;
     }
 }
 
